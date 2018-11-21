911ec9a1685f62daa9da04d1c13bef2d3f8e91a1
Syazwan Zamree <syazwanzamree@gmail.com>
2018-11-21 20:48:14 +0800

    add country db schema. #2

--- src/database/models/CountryModel.js
+++ src/database/models/CountryModel.js
@@ -1,1 +1,42 @@
-// country db model here
+import mongoose, { Schema } from 'mongoose';
+
+const CountrySchema = new Schema({
+  name: { type: String },
+  topLevelDomain: [{ type: String }],
+  alpha2Code: { type: String },
+  alpha3Code: { type: String },
+  callingCodes: [{ type: Number }],
+  capital: { type: String },
+  altSpellings: [{ type: String }],
+  region: { type: String },
+  subregion: { type: String },
+  population: { type: Number },
+  latlng: [{ type: Number }],
+  demonym: { type: String },
+  area: { type: Number },
+  gini: { type: Number },
+  timezones: [{ type: String }],
+  borders: [{ type: String }],
+  nativeName: { type: String },
+  numericCode: { type: Number },
+  currencies: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Currency',
+  }],
+  languages: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Language',
+  }],
+  translations: {
+    type: Schema.Types.ObjectId,
+    ref: 'Translation',
+  },
+  flag: { type: String },
+  regionalBlocs: [{
+    type: Schema.Types.ObjectId,
+    ref: 'RegionalBloc',
+  }],
+  cioc: { type: String },
+});
+
+export default mongoose.model('Country', CountrySchema);
