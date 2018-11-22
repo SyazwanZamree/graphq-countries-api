a14ddfac02dc86ae8292bd153af3c90c561656ab
Syazwan Zamree <syazwanzamree@gmail.com>
2018-11-22 08:02:12 +0800

    export models from db dir. #2

---
+++ src/database/models/index.js
@@ -0,0 +1,15 @@
+import Country from './CountryModel';
+import Currency from './CurrencyModel';
+import Language from './LanguageModel';
+import RegionalBloc from './RegionalBlocModel';
+import Translation from './TranslationModel';
+
+const models = {
+  Country,
+  Currency,
+  Language,
+  RegionalBloc,
+  Translation,
+};
+
+export default models;
